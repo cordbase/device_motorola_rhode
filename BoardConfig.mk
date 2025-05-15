LineageOS/android_device_motorola_rhode

rhode: disable brightness slider gamma conversion
Change-Id: Ia9a9d8e98053bcdbfccbee5fe2bdb0b030e7b60f

BoardConfig.mk
@@ -29,6 +29,7 @@ BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery

cordbase/device_motorola_rhode

BoardConfig.mk
@@ -29,6 +29,7 @@ BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
 
 # Properties
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery