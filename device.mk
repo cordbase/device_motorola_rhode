7c7a33aa29f92054853f2c6c434011293cb76bf5
cordbase <cordbase@users.noreply.github.com>
2025-10-15 17:34:45 +0530

    rhode: overlay: Add YAAP maintainer name

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+PRODUCT_PACKAGES += \
+    SettingsOverlayRhode
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
@@ -73,3 +76,6 @@ PRODUCT_COPY_FILES += \
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/rhode/rhode-vendor.mk)
+
+# Board Platform
+TARGET_BOARD_PLATFORM := bengal
